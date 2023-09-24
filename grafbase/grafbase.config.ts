8e0fc1f24639342ba518c764be5cf912050ecdec
Emmanuel Chukwuemeka <agusmart94@gmail.com>
2023-09-24 14:27:29 +0100

    adding grafbase

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,54 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+// Welcome to Grafbase!
+// Define your data models, integrate auth, permission rules, custom resolvers, search, and more with Grafbase.
+// Integrate Auth
+// https://grafbase.com/docs/auth
+//
+// const authProvider = auth.OpenIDConnect({
+//   issuer: process.env.ISSUER_URL ?? ''
+// })
+//
+// Define Data Models
+// https://grafbase.com/docs/database
+
+
+const user = g.model("User", {
+  name: g.string().length({min:2, max:20}),
+  email: g.email().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  gitHubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.relation(()=>project).optional()
+});
+
+const project = g.model("Project", {
+    title: g.string().length({ min: 3 }),
+    description: g.string(),
+    image: g.url(),
+    liveSiteUrl: g.url(),
+    gitHubUrl: g.url(),
+    category: g.string().search(),
+    createBy: g.relation(() => user),
+  });
+
+const comment = g.model("Comment", {
+  post: g.relation(project),
+  body: g.string(),
+  likes: g.int().default(0),
+  author: g.relation(() => user).optional(),
+});
+
+
+export default config({
+  schema: g
+  // Integrate Auth
+  // https://grafbase.com/docs/auth
+  // auth: {
+  //   providers: [authProvider],
+  //   rules: (rules) => {
+  //     rules.private()
+  //   }
+  // }
+})
